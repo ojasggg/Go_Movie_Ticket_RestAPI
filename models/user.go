e3fc30197f50202dbc02e5ca304a2663b3abecfa
Ojash Gurung <ojasggg@gmail.com>
2025-05-11 02:47:23 -0400

    fix: fixing user models

--- models/user.go
+++ models/user.go
@@ -4,10 +4,10 @@ import "time"
 
 type User struct {
 	ID int `json:"id"`
-	Username string `json:"username" binding:"required"`
-	Email string `josn:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Role string `json:"role" binding:"required"`
+	Username string `json:"username"`
+	Email string `josn:"email"`
+	Password string `json:"password"`
+	Role string `json:"role"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
@@ -16,9 +16,6 @@ type RegisterUser struct {
 	Username string `json:"username" binding:"required"`
 	Email string `josn:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Role string `json:"role" binding:"required"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type LoginUser struct {
