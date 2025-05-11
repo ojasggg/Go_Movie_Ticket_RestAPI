5d5a7c00f1a3004949a4bc78f41198fc68e739a2
Ojash Gurung <ojasggg@gmail.com>
2025-05-11 01:25:35 -0400

    feat: Implemented JWT tokens to check authentication

---
+++ utils/jwt.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(os.Getenv("secret_key"))
+
+type JWTClaim struct {
+	UserID int `json:"userId"`
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID int, role string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	claims := &JWTClaim{
+		UserID: userID, 
+		Role: role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ValidateToken(tokenString string) (*JWTClaim, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaim{}, func(token *jwt.Token) (interface{}, error){
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*JWTClaim)
+	if !ok || !token.Valid {
+		return nil, errors.New("Invalid Token")
+	}
+
+	return claims, nil
+}
